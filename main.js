0d82147cc53031f73d2d4ac4886137ed8c966373
Erick Fonseca <erick.peixoto@academico.ifpb.edu.br>
2022-09-04 15:51:46 -0300

    Estrutura de Repetição For

---
+++ main.js
@@ -0,0 +1,9 @@
+/* Este programa realiza a exibição de uma lista de 0 a 100
+pulando de 2 em 2 */
+
+for (let i = 0 ; i<= 100; i += 2) {
+    console.log(i)
+}
+
+/* o for possui por padrão uma estrutura iniciada por
+iniciação; expressão booleana(lógica); passo a ser dado */
